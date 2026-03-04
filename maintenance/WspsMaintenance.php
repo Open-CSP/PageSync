3f9aa43106cbbcf2e64530873419bffeb9384232
Marijn van Wezel <96489967+marijnvanwezel@users.noreply.github.com>
2026-03-04 15:53:43 +0100

    Do not terminate early when running the maintenance script
    This pull request ensures the script keeps running, even when an error occurs, and lists all errors at the end of the run.

--- maintenance/WspsMaintenance.php
+++ maintenance/WspsMaintenance.php
@@ -352,6 +352,8 @@ require_once "$IP/maintenance/Maintenance.php";
 			if ( $user->isAnon() ) {
 				$user->addToDatabase();
 			}
+
+			$errors = [];
 			foreach ( $indexFile as $indexFileEntry ) {
 				//echo "\nWorking on $indexFileEntry";
 				$ns = PSNameSpaceUtils::getNSFromTitleString( $indexFileEntry );
@@ -360,18 +362,25 @@ require_once "$IP/maintenance/Maintenance.php";
 				$pageId = PSCore::getPageIdFromTitle( $pageTitle );
 				//echo "\nPage ID : $pageId\n";
 
+				echo "Working on page id $pageId with user $userName on title $pageTitle\n";
 				$result = PSCore::addFileForExport(
 					$pageId,
 					$userName
 				);
 				if ( $result['status'] === false ) {
-					die( "ERROR: " . $result['info'] );
+					$errors[] = $result['info'];
 				}
 
-				echo "Working on page id $pageId with user $userName on title $pageTitle\n";
 				$cnt++;
 			}
-			echo "\n$cnt files Rebuild from Index.\nDone!\n";
+			echo "\n$cnt files rebuilt from index.\nDone!\n";
+
+			$errCnt = count( $errors );
+			if ( $errCnt > 0 ) {
+                echo "WARNING: $errCnt error(s) occurred:\n";
+				echo implode( "\n", $errors ) . "\n";
+			}
+
 			die();
 		}
 
